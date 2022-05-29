d41a70bcc4ad78c80d05374d37e58157dbcc4218
unknown <enter7377@naver.com>
2022-05-29 17:16:52 +0900

    fix: that was close

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb"
 
 const MongoDBClient = new MongoClient(
-  "mongodb://rw:gy26kyGbGrrvOCGx@cluster0-shard-00-00.aahcp.mongodb.net:27017,cluster0-shard-00-01.aahcp.mongodb.net:27017,cluster0-shard-00-02.aahcp.mongodb.net:27017/?ssl=true&replicaSet=atlas-qb8tfk-shard-0&authSource=admin&retryWrites=true&w=majority"
+  process.env.DB_URI ?? ""
 )
 
 export const connection = MongoDBClient.connect()
