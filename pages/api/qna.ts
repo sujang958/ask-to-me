ad7248d165dc1866390ff780d1545068bd7455bc
unknown <enter7377@naver.com>
2022-05-29 17:29:35 +0900

    feat: implement adding questions

--- pages/api/qna.ts
+++ pages/api/qna.ts
@@ -11,7 +11,9 @@ const Qna: NextApiHandler = async (req, res) => {
         .toArray()
     )
   } else {
+    if (!("question" in req.body)) return res.status(401)
     const question = req.body.question
+    if (question.length < 1) return res.status(401)
     const { insertedId } = await collection.insertOne({
       question,
       answered: false,
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,80 +7,112 @@ const Home: NextPage = () => {
   const [currentScreen, setCurrentScreen] = useState<Screens>("ANSWERED")
   const [isLoading, setLoading] = useState(false)
   const [qnas, setQnas] = useState<Qna[]>([])
+  const [question, setQuestion] = useState("")
+  const getQnas = async () => {
+    setLoading(true)
+    const res = await fetch("/api/qna")
+    const json = await res.json()
+    setQnas(json)
+    setLoading(false)
+  }
+  const onQuestionButtonClick = async () => {
+    await fetch("/api/qna", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      } as any,
+      body: JSON.stringify({ question }),
+    })
+    getQnas()
+  }
   useEffect(() => {
-    const getQnas = async () => {
-      setLoading(true)
-      const res = await fetch("/api/qna")
-      const json = await res.json()
-      setQnas(json)
-      setLoading(false)
-    }
     getQnas()
   }, [])
 
   if (isLoading)
     return (
-      <div className="flex w-full flex-col items-center justify-center">
-        <p className="text-2xl font-bold">로딩중이다 샌즈</p>
+      <div className="flex h-screen w-full flex-col items-center justify-center">
+        <p className="text-5xl font-bold">로딩중이다 샌즈</p>
       </div>
     )
 
   return (
-    <div className="flex min-h-screen w-full flex-col rounded-t-lg bg-slate-50">
-      <header className="flex w-full flex-row items-center">
-        <div
-          className="relative flex-1 cursor-pointer py-4 text-center"
-          onClick={() => setCurrentScreen("ANSWERED")}
-        >
-          <p className="text-2xl font-semibold">답변된 질문</p>
-          {currentScreen === "ANSWERED" && (
-            <motion.div
-              layoutId="REPLY_HEADER"
-              initial={false}
-              className="absolute bottom-0 h-[2px] w-full bg-slate-500"
-            ></motion.div>
-          )}
+    <div className="flex min-h-screen w-full flex-col items-center font-pretendard">
+      <div className="flex w-full max-w-3xl flex-col">
+        <header className="flex w-full flex-col items-center py-12 px-6">
+          <p className="text-3xl font-bold">ㅈ성훈한테 물어보기</p>
+          <div className="flex w-full flex-col items-center py-6 px-2">
+            <textarea
+              onChange={({ currentTarget: { value } }) => setQuestion(value)}
+              placeholder="질문할 내용을 입력하1세요"
+              rows={4}
+              className="min-h-[12rem] w-[45%] rounded-lg border border-slate-300 py-1.5 px-3 text-lg outline-none"
+            ></textarea>
+            <button
+              className="mt-8 rounded-lg bg-violet-600 py-2 px-5 text-lg font-bold text-white"
+              onClick={onQuestionButtonClick}
+            >
+              질1문하기
+            </button>
+          </div>
+        </header>
+        <div className="flex min-h-screen w-full flex-col rounded-t-lg bg-slate-50">
+          <header className="flex w-full flex-row items-center">
+            <div
+              className="relative flex-1 cursor-pointer py-4 text-center"
+              onClick={() => setCurrentScreen("ANSWERED")}
+            >
+              <p className="text-2xl font-semibold">답변된 질문</p>
+              {currentScreen === "ANSWERED" && (
+                <motion.div
+                  layoutId="REPLY_HEADER"
+                  initial={false}
+                  className="absolute bottom-0 h-[2px] w-full bg-slate-500"
+                ></motion.div>
+              )}
+            </div>
+            <div
+              className="relative flex-1 cursor-pointer py-4 text-center"
+              onClick={() => setCurrentScreen("UNANSWERED")}
+            >
+              <p className="text-2xl font-semibold">답변되지 않1은 질문</p>
+              {currentScreen === "UNANSWERED" && (
+                <motion.div
+                  layoutId="REPLY_HEADER"
+                  initial={false}
+                  className="absolute bottom-0 h-[2px] w-full bg-slate-500"
+                ></motion.div>
+              )}
+            </div>
+          </header>
+          <main className="flex flex-col py-6 px-4">
+            {currentScreen === "ANSWERED" &&
+              qnas
+                .filter(({ answered }) => answered)
+                .map((question, i) => (
+                  <div className="flex flex-col py-6 px-4" key={i}>
+                    <p className="text-3xl font-semibold">
+                      &gt; {question.question}
+                    </p>
+                    <p className="py-2 px-1 text-xl font-medium">{`< ${question.answer}`}</p>
+                    <p className="pt-2 text-sm text-slate-500">
+                      {question.answeredAt}
+                    </p>
+                  </div>
+                ))}
+            {currentScreen === "UNANSWERED" &&
+              qnas
+                .filter(({ answered }) => !answered)
+                .map((question, i) => (
+                  <div className="flex flex-col py-6 px-4" key={i}>
+                    <p className="text-3xl font-semibold">
+                      &gt; {question.question}
+                    </p>
+                  </div>
+                ))}
+          </main>
         </div>
-        <div
-          className="relative flex-1 cursor-pointer py-4 text-center"
-          onClick={() => setCurrentScreen("UNANSWERED")}
-        >
-          <p className="text-2xl font-semibold">답변되지 않1은 질문</p>
-          {currentScreen === "UNANSWERED" && (
-            <motion.div
-              layoutId="REPLY_HEADER"
-              initial={false}
-              className="absolute bottom-0 h-[2px] w-full bg-slate-500"
-            ></motion.div>
-          )}
-        </div>
-      </header>
-      <main className="flex flex-col py-6 px-4">
-        {currentScreen === "ANSWERED" &&
-          qnas
-            .filter(({ answered }) => answered)
-            .map((question, i) => (
-              <div className="flex flex-col py-6 px-4" key={i}>
-                <p className="text-3xl font-semibold">
-                  &gt; {question.question}
-                </p>
-                <p className="py-2 px-1 text-xl font-medium">{`< ${question.answer}`}</p>
-                <p className="pt-2 text-sm text-slate-500">
-                  {question.answeredAt}
-                </p>
-              </div>
-            ))}
-        {currentScreen === "UNANSWERED" &&
-          qnas
-            .filter(({ answered }) => !answered)
-            .map((question, i) => (
-              <div className="flex flex-col py-6 px-4" key={i}>
-                <p className="text-3xl font-semibold">
-                  &gt; {question.question}
-                </p>
-              </div>
-            ))}
-      </main>
+      </div>
     </div>
   )
 }
