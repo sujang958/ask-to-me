d2bbeba320da2ec8bccd66a9557206d77bfb207c
unknown <enter7377@naver.com>
2022-05-29 15:29:52 +0900

    feat: add shared header animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,44 @@
 import { NextPage } from "next"
+import { useState } from "react"
+import { motion } from "framer-motion"
+
+type Screens = "ANSWERED" | "UNANSWERED"
 
 const Home: NextPage = () => {
-  return <p className="font-pretendard text-6xl">hi</p>
+  const [currentScreen, setCurrentScreen] = useState<Screens>("ANSWERED")
+
+  return (
+    <div className="flex min-h-screen w-full flex-col rounded-t-lg bg-slate-50">
+      <header className="flex w-full flex-row items-center">
+        <div
+          className="relative flex-1 cursor-pointer py-4 text-center"
+          onClick={() => setCurrentScreen("ANSWERED")}
+        >
+          <p className="text-2xl font-semibold">답변된 질문</p>
+          {currentScreen === "ANSWERED" && (
+            <motion.div
+              layoutId="REPLY_HEADER"
+              initial={false}
+              className="absolute bottom-0 h-[2px] w-full bg-slate-500"
+            ></motion.div>
+          )}
+        </div>
+        <div
+          className="relative flex-1 cursor-pointer py-4 text-center"
+          onClick={() => setCurrentScreen("UNANSWERED")}
+        >
+          <p className="text-2xl font-semibold">답변되지 않1은 질문</p>
+          {currentScreen === "UNANSWERED" && (
+            <motion.div
+              layoutId="REPLY_HEADER"
+              initial={false}
+              className="absolute bottom-0 h-[2px] w-full bg-slate-500"
+            ></motion.div>
+          )}
+        </div>
+      </header>
+    </div>
+  )
 }
 
 export default Home
