4e431f24b5182f7bc7ad7f9d2833ebd8dc6ebf33
unknown <enter7377@naver.com>
2022-05-29 17:09:25 +0900

    feat: add getting only answered qna

--- pages/api/qna.ts
+++ pages/api/qna.ts
@@ -5,7 +5,11 @@ const Qna: NextApiHandler = async (req, res) => {
   await connection
   const collection = getCollection()
   if (req.method === "GET") {
-    return res.json(await collection.find().toArray())
+    return res.json(
+      await collection
+        .find("answered" in req.query ? { answered: true } : {})
+        .toArray()
+    )
   } else {
     const question = req.body.question
     const { insertedId } = await collection.insertOne({
